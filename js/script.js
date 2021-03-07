38ae4248823e58663fe134865a2303f77bd10cca
Jordan Laor <jordanlaor@gmail.com>
2021-03-07 10:19:05 +0200

    made sure only btns click will work

--- js/script.js
+++ js/script.js
@@ -150,198 +150,202 @@ function currentGraphBtn(targetBtn) {
 }
 
 async function countryClick(e) {
-  if (currentGraph !== e.target.textContent) {
-    currentGraph = e.target.textContent;
-    currentGraphBtn(e.target);
-    try {
-      const covidData = await fetchData(
-        `${urls.covid}/${world[countries[e.target.textContent]][e.target.textContent].code}`
-      );
-      const labels = [];
-      const dataCritical = [];
-      const dataConfirmed = [];
-      const dataNewConfirmed = [];
-      const dataDeaths = [];
-      const dataNewDeaths = [];
-      const dataRecovered = [];
-      const dataNewRecovered = [];
-      covidData.data.timeline.forEach((update) => {
-        labels.push(new Date(update.date));
-        dataConfirmed.push({ x: new Date(update.date), y: update.confirmed });
-        dataNewConfirmed.push({ x: new Date(update.date), y: update.new_confirmed });
-        dataDeaths.push({ t: new Date(update.date), y: update.deaths });
-        dataNewDeaths.push({ t: new Date(update.date), y: update.new_deaths });
-        dataRecovered.push({ t: new Date(update.date), y: update.recovered });
-        dataNewRecovered.push({ t: new Date(update.date), y: update.new_recovered });
-      });
-      dataCritical.push({
-        t: new Date(covidData.data.updated_at.match(/\d{4}-\d{2}-\d{2}/g)),
-        y: covidData.data.latest_data.critical,
-      });
+  if (e.target.classList.contains('btn')) {
+    if (currentGraph !== e.target.textContent) {
+      currentGraph = e.target.textContent;
+      currentGraphBtn(e.target);
+      try {
+        const covidData = await fetchData(
+          `${urls.covid}/${world[countries[e.target.textContent]][e.target.textContent].code}`
+        );
+        const labels = [];
+        const dataCritical = [];
+        const dataConfirmed = [];
+        const dataNewConfirmed = [];
+        const dataDeaths = [];
+        const dataNewDeaths = [];
+        const dataRecovered = [];
+        const dataNewRecovered = [];
+        covidData.data.timeline.forEach((update) => {
+          labels.push(new Date(update.date));
+          dataConfirmed.push({ x: new Date(update.date), y: update.confirmed });
+          dataNewConfirmed.push({ x: new Date(update.date), y: update.new_confirmed });
+          dataDeaths.push({ t: new Date(update.date), y: update.deaths });
+          dataNewDeaths.push({ t: new Date(update.date), y: update.new_deaths });
+          dataRecovered.push({ t: new Date(update.date), y: update.recovered });
+          dataNewRecovered.push({ t: new Date(update.date), y: update.new_recovered });
+        });
+        dataCritical.push({
+          t: new Date(covidData.data.updated_at.match(/\d{4}-\d{2}-\d{2}/g)),
+          y: covidData.data.latest_data.critical,
+        });
 
-      if (covidData.data.timeline.length === 0) {
-        handleError('There is no data available');
+        if (covidData.data.timeline.length === 0) {
+          handleError('There is no data available');
+        }
+        const datasets = [
+          {
+            label: 'Total Confirmed',
+            data: fromDayTo10(dataConfirmed),
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(255, 99, 132, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'New Confirmed',
+            data: fromDayTo10(dataNewConfirmed),
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(193, 37, 83)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'Total Recovered',
+            data: fromDayTo10(dataRecovered),
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(54, 162, 235, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'New Recovered',
+            data: fromDayTo10(dataNewRecovered),
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(0, 111, 179, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'Total Deaths',
+            data: fromDayTo10(dataDeaths),
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(75, 192, 192, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'New Deaths',
+            data: fromDayTo10(dataNewDeaths),
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(0, 138, 139, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'Total Critical',
+            data: dataCritical,
+            showLine: false,
+            type: 'bar',
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(255, 206, 86, 1)'],
+            borderWidth: 5,
+          },
+        ];
+
+        drawChart('line', fromDayTo10(labels), datasets, options.date);
+      } catch (error) {
+        handleError(error);
       }
-      const datasets = [
-        {
-          label: 'Total Confirmed',
-          data: fromDayTo10(dataConfirmed),
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(255, 99, 132, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'New Confirmed',
-          data: fromDayTo10(dataNewConfirmed),
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(193, 37, 83)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'Total Recovered',
-          data: fromDayTo10(dataRecovered),
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(54, 162, 235, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'New Recovered',
-          data: fromDayTo10(dataNewRecovered),
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(0, 111, 179, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'Total Deaths',
-          data: fromDayTo10(dataDeaths),
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(75, 192, 192, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'New Deaths',
-          data: fromDayTo10(dataNewDeaths),
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(0, 138, 139, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'Total Critical',
-          data: dataCritical,
-          showLine: false,
-          type: 'bar',
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(255, 206, 86, 1)'],
-          borderWidth: 5,
-        },
-      ];
-
-      drawChart('line', fromDayTo10(labels), datasets, options.date);
-    } catch (error) {
-      handleError(error);
     }
   }
 }
 
 async function continentClick(e) {
-  if (currentGraph !== e.target.textContent) {
-    currentGraph = e.target.textContent;
-    currentGraphBtn(e.target);
-    try {
-      const covidData = await fetchData(urls.covid);
-      const btnsCountry = document.querySelector('.btns__country');
-      const labels = [];
-      const dataConfirmed = [];
-      const dataDeaths = [];
-      const dataRecovered = [];
-      const dataCritical = [];
-      resetBtns('country');
-      if (e.target.textContent === 'World') {
-        Object.keys(world).forEach((continent) => {
-          labels.push(continent);
-          let dataConfirmedContinent;
-          let dataDeathsContinent;
-          let dataRecoveredContinent;
-          let dataCriticalContinent;
-          for (const key of Object.keys(world[continent])) {
-            dataConfirmedContinent = 0;
-            dataDeathsContinent = 0;
-            dataRecoveredContinent = 0;
-            dataCriticalContinent = 0;
+  if (e.target.classList.contains('btn')) {
+    if (currentGraph !== e.target.textContent) {
+      currentGraph = e.target.textContent;
+      currentGraphBtn(e.target);
+      try {
+        const covidData = await fetchData(urls.covid);
+        const btnsCountry = document.querySelector('.btns__country');
+        const labels = [];
+        const dataConfirmed = [];
+        const dataDeaths = [];
+        const dataRecovered = [];
+        const dataCritical = [];
+        resetBtns('country');
+        if (e.target.textContent === 'World') {
+          Object.keys(world).forEach((continent) => {
+            labels.push(continent);
+            let dataConfirmedContinent;
+            let dataDeathsContinent;
+            let dataRecoveredContinent;
+            let dataCriticalContinent;
+            for (const key of Object.keys(world[continent])) {
+              dataConfirmedContinent = 0;
+              dataDeathsContinent = 0;
+              dataRecoveredContinent = 0;
+              dataCriticalContinent = 0;
+              if (key === 'type') {
+                continue;
+              }
+              const covidCountryData = covidData.data.find((data) => data.name === key);
+              if (!covidCountryData) {
+                delete world[continent][key];
+                continue;
+              }
+              dataConfirmedContinent += covidCountryData.latest_data.confirmed;
+              dataCriticalContinent += covidCountryData.latest_data.critical;
+              dataDeathsContinent += covidCountryData.latest_data.deaths;
+              dataRecoveredContinent += covidCountryData.latest_data.recovered;
+            }
+            dataConfirmed.push(dataConfirmedContinent);
+            dataCritical.push(dataCriticalContinent);
+            dataDeaths.push(dataDeathsContinent);
+            dataRecovered.push(dataRecoveredContinent);
+            drawBtns(world[continent], 'country');
+          });
+          btnsCountry.addEventListener('click', countryClick);
+        } else {
+          for (const key of Object.keys(world[e.target.textContent])) {
             if (key === 'type') {
               continue;
             }
             const covidCountryData = covidData.data.find((data) => data.name === key);
             if (!covidCountryData) {
-              delete world[continent][key];
+              delete world[e.target.textContent][key];
               continue;
             }
-            dataConfirmedContinent += covidCountryData.latest_data.confirmed;
-            dataCriticalContinent += covidCountryData.latest_data.critical;
-            dataDeathsContinent += covidCountryData.latest_data.deaths;
-            dataRecoveredContinent += covidCountryData.latest_data.recovered;
+            labels.push(key);
+            dataConfirmed.push(covidCountryData.latest_data.confirmed);
+            dataCritical.push(covidCountryData.latest_data.critical);
+            dataDeaths.push(covidCountryData.latest_data.deaths);
+            dataRecovered.push(covidCountryData.latest_data.recovered);
           }
-          dataConfirmed.push(dataConfirmedContinent);
-          dataCritical.push(dataCriticalContinent);
-          dataDeaths.push(dataDeathsContinent);
-          dataRecovered.push(dataRecoveredContinent);
-          drawBtns(world[continent], 'country');
-        });
-        btnsCountry.addEventListener('click', countryClick);
-      } else {
-        for (const key of Object.keys(world[e.target.textContent])) {
-          if (key === 'type') {
-            continue;
-          }
-          const covidCountryData = covidData.data.find((data) => data.name === key);
-          if (!covidCountryData) {
-            delete world[e.target.textContent][key];
-            continue;
-          }
-          labels.push(key);
-          dataConfirmed.push(covidCountryData.latest_data.confirmed);
-          dataCritical.push(covidCountryData.latest_data.critical);
-          dataDeaths.push(covidCountryData.latest_data.deaths);
-          dataRecovered.push(covidCountryData.latest_data.recovered);
+          drawBtns(world[e.target.textContent], 'country');
+          btnsCountry.addEventListener('click', countryClick);
         }
-        drawBtns(world[e.target.textContent], 'country');
-        btnsCountry.addEventListener('click', countryClick);
+
+        const datasets = [
+          {
+            label: 'Total Confirmed',
+            data: dataConfirmed,
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(255, 99, 132, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'Total Recovered',
+            data: dataRecovered,
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(54, 162, 235, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'Total Critical',
+            data: dataCritical,
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(255, 206, 86, 1)'],
+            borderWidth: 3,
+          },
+          {
+            label: 'Total Deaths',
+            data: dataDeaths,
+            backgroundColor: ['transparent'],
+            borderColor: ['rgba(75, 192, 192, 1)'],
+            borderWidth: 3,
+          },
+        ];
+
+        drawChart('line', labels, datasets, options.normal);
+      } catch (error) {
+        handleError(error);
       }
-
-      const datasets = [
-        {
-          label: 'Total Confirmed',
-          data: dataConfirmed,
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(255, 99, 132, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'Total Recovered',
-          data: dataRecovered,
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(54, 162, 235, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'Total Critical',
-          data: dataCritical,
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(255, 206, 86, 1)'],
-          borderWidth: 3,
-        },
-        {
-          label: 'Total Deaths',
-          data: dataDeaths,
-          backgroundColor: ['transparent'],
-          borderColor: ['rgba(75, 192, 192, 1)'],
-          borderWidth: 3,
-        },
-      ];
-
-      drawChart('line', labels, datasets, options.normal);
-    } catch (error) {
-      handleError(error);
     }
   }
 }
